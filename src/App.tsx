cbb7aeb20dd762112db6afc51f3f69fa9a92642b
Johannes Damp <jodamp@uni-mainz.de>
2023-03-07 10:44:49 +0100

    Change indents from 2 to 4 spaces

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,23 @@ import RollButton from './RollButton';
 
 
 function allNewDice(): Array<number> {
-  return Array.from({length: 10}, () => Math.ceil(Math.random()*6));
+    return Array.from({length: 10}, () => Math.ceil(Math.random()*6));
 }
 
 
 export default function App() {
-  const [dieValues, setDieValues] = React.useState(allNewDice());
-  function buttonClick() {
-    setDieValues(allNewDice);
+    const [dieValues, setDieValues] = React.useState(allNewDice());
+    function buttonClick() {
+        setDieValues(allNewDice);
   }
 
-  const dies = dieValues.map(val => <Die value={val}/>)
+  const dies = dieValues.map(val => <Die value={val}/>);
   return (
-  <main>
-    <div className='dieContainer'>
-    {dies}
-    </div>
-    <RollButton clickHandler={buttonClick}/>
-  </main>)
+    <main>
+        <div className='dieContainer'>
+            {dies}
+        </div>
+        <RollButton clickHandler={buttonClick}/>
+    </main>
+    );
 }
-
-
